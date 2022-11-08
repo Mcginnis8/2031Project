aa3a004df0dd951b41f8eded0736469d4be9d401
Cole McGinnis <82854743+Mcginnis8@users.noreply.github.com>
2022-11-08 17:41:34 -0500

    Beep Test Test Updated

--- BeepTest.asm
+++ BeepTest.asm
@@ -3,45 +3,211 @@
 ; tone generator peripheral once per second.
 
 ORG 0
-
-	; Get the switch values
-	ADDI	70
-	; Send to the peripheral
-	OUT    Beep
-	; Delay for 1 second
-	CALL   Delay
-	ADDI	5
-	; Send to the peripheral
-	OUT    Beep
-	; Delay for 1 second
-	CALL   Delay
-	ADDI	4
-	; Send to the peripheral
-	OUT    Beep
-	; Delay for 1 second
-	CALL   Delay
-	ADDI	6
-	; Send to the peripheral
-	OUT    Beep
-	; Delay for 1 second
-	CALL   Delay
-	ADDI	3
-	; Send to the peripheral
-	OUT    Beep
-	; Delay for 1 second
-	CALL   Delay
-	; Do it again
-	ADDI	5
-	; Send to the peripheral
-	OUT    Beep
-	; Delay for 1 second
-	CALL   Delay
-	ADDI	6
-	; Send to the peripheral
-	OUT    Beep
-	; Delay for 1 second
-	CALL   Delay
-	JUMP 0
+ADDI  71
+OUT    Beep
+CALL   Delay
+ADDI  4
+OUT    Beep
+CALL   Delay
+ADDI  5
+OUT    Beep
+CALL   Delay
+ADDI  4
+OUT    Beep
+CALL   Delay
+ADDI  5
+OUT    Beep
+CALL   Delay
+ADDI  6
+OUT    Beep
+CALL   Delay
+ADDI  5
+OUT    Beep
+CALL   Delay
+ADDI  6
+OUT    Beep
+CALL   Delay
+ADDI  7
+OUT    Beep
+CALL   Delay
+ADDI  6
+OUT    Beep
+CALL   Delay
+ADDI  7
+OUT    Beep
+CALL   Delay
+ADDI  8
+OUT    Beep
+CALL   Delay
+ADDI  8
+OUT    Beep
+CALL   Delay
+ADDI  8
+OUT    Beep
+CALL   Delay
+ADDI  9
+OUT    Beep
+CALL   Delay
+ADDI  10
+OUT    Beep
+CALL   Delay
+ADDI  10
+OUT    Beep
+CALL   Delay
+ADDI  10
+OUT    Beep
+CALL   Delay
+ADDI  11
+OUT    Beep
+CALL   Delay
+ADDI  12
+OUT    Beep
+CALL   Delay
+ADDI  13
+OUT    Beep
+CALL   Delay
+ADDI  13
+OUT    Beep
+CALL   Delay
+ADDI  15
+OUT    Beep
+CALL   Delay
+ADDI  15
+OUT    Beep
+CALL   Delay
+ADDI  16
+OUT    Beep
+CALL   Delay
+ADDI  16
+OUT    Beep
+CALL   Delay
+ADDI  18
+OUT    Beep
+CALL   Delay
+ADDI  19
+OUT    Beep
+CALL   Delay
+ADDI  20
+OUT    Beep
+CALL   Delay
+ADDI  21
+OUT    Beep
+CALL   Delay
+ADDI  23
+OUT    Beep
+CALL   Delay
+ADDI  24
+OUT    Beep
+CALL   Delay
+ADDI  25
+OUT    Beep
+CALL   Delay
+ADDI  27
+OUT    Beep
+CALL   Delay
+ADDI  28
+OUT    Beep
+CALL   Delay
+ADDI  30
+OUT    Beep
+CALL   Delay
+ADDI  32
+OUT    Beep
+CALL   Delay
+ADDI  34
+OUT    Beep
+CALL   Delay
+ADDI  35
+OUT    Beep
+CALL   Delay
+ADDI  38
+OUT    Beep
+CALL   Delay
+ADDI  40
+OUT    Beep
+CALL   Delay
+ADDI  43
+OUT    Beep
+CALL   Delay
+ADDI  45
+OUT    Beep
+CALL   Delay
+ADDI  48
+OUT    Beep
+CALL   Delay
+ADDI  50
+OUT    Beep
+CALL   Delay
+ADDI  54
+OUT    Beep
+CALL   Delay
+ADDI  56
+OUT    Beep
+CALL   Delay
+ADDI  60
+OUT    Beep
+CALL   Delay
+ADDI  64
+OUT    Beep
+CALL   Delay
+ADDI  67
+OUT    Beep
+CALL   Delay
+ADDI  72
+OUT    Beep
+CALL   Delay
+ADDI  76
+OUT    Beep
+CALL   Delay
+ADDI  80
+OUT    Beep
+CALL   Delay
+ADDI  85
+OUT    Beep
+CALL   Delay
+ADDI  90
+OUT    Beep
+CALL   Delay
+ADDI  95
+OUT    Beep
+CALL   Delay
+ADDI  101
+OUT    Beep
+CALL   Delay
+ADDI  107
+OUT    Beep
+CALL   Delay
+ADDI  114
+OUT    Beep
+CALL   Delay
+ADDI  120
+OUT    Beep
+CALL   Delay
+ADDI  127
+OUT    Beep
+CALL   Delay
+ADDI  135
+OUT    Beep
+CALL   Delay
+ADDI  143
+OUT    Beep
+CALL   Delay
+ADDI  151
+OUT    Beep
+CALL   Delay
+ADDI  161
+OUT    Beep
+CALL   Delay
+ADDI  170
+OUT    Beep
+CALL   Delay
+ADDI  180
+OUT    Beep
+CALL   Delay
+ADDI  190
+OUT    Beep
+CALL   Delay
+JUMP 0
 
 ; Subroutine to delay for 0.2 seconds.
 Delay:
